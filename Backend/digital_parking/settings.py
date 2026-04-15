a8efd16ad9d7ae0ecb7e4220e8ec7e57cf562a4e
nafijur-rahaman <20234103116@cse.bubt.edu.bd>
2026-04-15 23:41:13 +0600

    setup for deploy

--- Backend/digital_parking/settings.py
+++ Backend/digital_parking/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-cjf=&%pl4ys#^=7bb3d+i8t^3z@ae@rleib8she8poblytv3vv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -133,7 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -168,3 +172,4 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
 ]
+
